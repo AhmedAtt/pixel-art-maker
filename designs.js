5fdde0423557affa2d413e3b0dd2b32f91eb74aa
AhmedAtt <ahmedfattia@outlook.com>
2017-11-23 11:17:33 +0200

    Made some improvements

--- designs.js
+++ designs.js
@@ -1,41 +1,32 @@
-$(document).ready(function (){
-  $('#btn-submit').click(function (){
-    console.log('Button pressed');
-    var height =$('#input_height').val();
-    var width =$('#input_width').val();
-    console.log('h ='+height);
-    console.log('w  ='+width);
-    makeGrid(width,height);
+$(document).ready(function() {
+
+  //Fire the makeGrid Function when submit is clicked
+
+  $('#btn-submit').click(function() {
+    var height = $('#input_height').val();
+    var width = $('#input_width').val();
+    makeGrid(width, height);
   });
 
-  function makeGrid(w,h) {
+  //Function that generates Grid based on user (width,height) input
+  function makeGrid(w, h) {
     $('#pixel_canvas tr').remove();
     console.log("Intiating Grid");
-    for(var i=0;i<h;i++){
-      var row="<tr>";
-      for(var j=0;j<w;j++){
-        row+="<td></td>"
+    for (var i = 0; i < h; i++) {
+      var row = "<tr>";
+      for (var j = 0; j < w; j++) {
+        row += "<td></td>"
       }
-      row+="</tr>";
+      row += "</tr>";
       $('#pixel_canvas').append(row);
     }
+
     //Assign Click event to a table cell
-    $("td").click(function(){
-        color =$('#colorPicker').val();
-        console.log(color);
-
-        var column = $(this).parent().children().index(this);
-        var row = $(this).parent().parent().children().index(this.parentNode);
-
-        var table=document.getElementById('pixel_canvas');
-        var currentRow=table.rows[row];
-        var cell = currentRow.cells[column];
-
-        $(cell).css({
-          'background-color': color
-        });
+    $("td").click(function() {
+      color = $('#colorPicker').val();
+      $(this).css({
+        'background-color': color
+      });
     });
   }
-
-
 });
